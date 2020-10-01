cdd99c03e0bfeaedbd3c73d5263edc552fadadb7
Duncan Morrison <duncanmorrison2001@yahoo.com>
2020-10-01 15:00:20 +0100

    all tests passing

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,35 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+  it 'returns 1 when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+
+  it 'returns 22 when passed 22' do
+    expect(fizzbuzz(22)).to eq 22
+  end
+
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'return "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+
+  it 'retuns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
+  it 'returns "fizzbuzz" when passed 30' do
+    expect(fizzbuzz(30)).to eq "fizzbuzz"
   end
 end
